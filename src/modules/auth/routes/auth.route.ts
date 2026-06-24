1775f6bfc8e2feeb3dc325684b8207a076657e89
Muhammed Sajid S <sajid189210@gmail.com>
2026-06-25 00:09:48 +0530

    Both route files now import Request, Response, NextFunction from express and explicitly type all handler parameters

--- src/modules/auth/routes/auth.route.ts
+++ src/modules/auth/routes/auth.route.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { ROUTE_PATHS } from '../../../core/domain/constants/routes';
 import AuthController from '../controllers/auth.controller';
 
@@ -6,10 +6,10 @@ import AuthController from '../controllers/auth.controller';
 const buildAuthRouter = (authController: AuthController) => {
     const authRouter = Router();
 
-    authRouter.post(ROUTE_PATHS.AUTH.SIGNUP, (req, res, next) => authController.signup(req, res, next));
-    authRouter.post(ROUTE_PATHS.AUTH.LOGIN, (req, res, next) => authController.login(req, res, next));
-    authRouter.post(ROUTE_PATHS.AUTH.REFRESH_TOKEN, (req, res, next) => authController.validateRefreshToken(req, res, next));
-    authRouter.post(ROUTE_PATHS.AUTH.LOGOUT, (req, res, next) => authController.logout(req, res, next));
+    authRouter.post(ROUTE_PATHS.AUTH.SIGNUP, (req: Request, res: Response, next: NextFunction) => authController.signup(req, res, next));
+    authRouter.post(ROUTE_PATHS.AUTH.LOGIN, (req: Request, res: Response, next: NextFunction) => authController.login(req, res, next));
+    authRouter.post(ROUTE_PATHS.AUTH.REFRESH_TOKEN, (req: Request, res: Response, next: NextFunction) => authController.validateRefreshToken(req, res, next));
+    authRouter.post(ROUTE_PATHS.AUTH.LOGOUT, (req: Request, res: Response, next: NextFunction) => authController.logout(req, res, next));
 
     return authRouter;
 }
--- src/modules/posts/routes/post.route.ts
+++ src/modules/posts/routes/post.route.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { ROUTE_PATHS } from '../../../core/domain/constants/routes';
 import PostController from '../controller/post.controller';
 import upload from '../../../core/middleware/upload.middleware';
@@ -7,18 +7,18 @@ import { authMiddleware } from '../../../core/middleware/auth.middleware';
 const buildPostRoutes = (postController: PostController) => {
     const postRouter = Router();
 
-    postRouter.get(ROUTE_PATHS.POST.PUBLISHED, (req, res, next) => postController.getAllPublishedPosts(req, res, next));
-    postRouter.get(ROUTE_PATHS.POST.SEARCH, (req, res, next) => postController.searchPosts(req, res, next));
-    postRouter.get(ROUTE_PATHS.POST.GET_BY_AUTHOR, (req, res, next) => postController.getPostsByAuthorId(req, res, next));
+    postRouter.get(ROUTE_PATHS.POST.PUBLISHED, (req: Request, res: Response, next: NextFunction) => postController.getAllPublishedPosts(req, res, next));
+    postRouter.get(ROUTE_PATHS.POST.SEARCH, (req: Request, res: Response, next: NextFunction) => postController.searchPosts(req, res, next));
+    postRouter.get(ROUTE_PATHS.POST.GET_BY_AUTHOR, (req: Request, res: Response, next: NextFunction) => postController.getPostsByAuthorId(req, res, next));
 
-    postRouter.get(ROUTE_PATHS.LIKED.ROOT, authMiddleware, (req, res, next) => postController.getLikedPosts(req, res, next));
-    postRouter.get(ROUTE_PATHS.LIKED.IDS, authMiddleware, (req, res, next) => postController.getLikedIds(req, res, next));
-    postRouter.post(ROUTE_PATHS.LIKED.TOGGLE, authMiddleware, (req, res, next) => postController.toggleLike(req, res, next));
+    postRouter.get(ROUTE_PATHS.LIKED.ROOT, authMiddleware, (req: Request, res: Response, next: NextFunction) => postController.getLikedPosts(req, res, next));
+    postRouter.get(ROUTE_PATHS.LIKED.IDS, authMiddleware, (req: Request, res: Response, next: NextFunction) => postController.getLikedIds(req, res, next));
+    postRouter.post(ROUTE_PATHS.LIKED.TOGGLE, authMiddleware, (req: Request, res: Response, next: NextFunction) => postController.toggleLike(req, res, next));
 
-    postRouter.get(ROUTE_PATHS.POST.GET_BY_ID, (req, res, next) => postController.getPostById(req, res, next));
-    postRouter.post(ROUTE_PATHS.POST.CREATE, authMiddleware, upload.single('image'), (req, res, next) => postController.createPost(req, res, next));
-    postRouter.put(ROUTE_PATHS.POST.UPDATE, authMiddleware, upload.single('image'), (req, res, next) => postController.updatePost(req, res, next));
-    postRouter.delete(ROUTE_PATHS.POST.DELETE, authMiddleware, (req, res, next) => postController.deletePost(req, res, next));
+    postRouter.get(ROUTE_PATHS.POST.GET_BY_ID, (req: Request, res: Response, next: NextFunction) => postController.getPostById(req, res, next));
+    postRouter.post(ROUTE_PATHS.POST.CREATE, authMiddleware, upload.single('image'), (req: Request, res: Response, next: NextFunction) => postController.createPost(req, res, next));
+    postRouter.put(ROUTE_PATHS.POST.UPDATE, authMiddleware, upload.single('image'), (req: Request, res: Response, next: NextFunction) => postController.updatePost(req, res, next));
+    postRouter.delete(ROUTE_PATHS.POST.DELETE, authMiddleware, (req: Request, res: Response, next: NextFunction) => postController.deletePost(req, res, next));
 
     return postRouter;
 }
